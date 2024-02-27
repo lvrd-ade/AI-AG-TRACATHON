0c1e1d3e5e930a7ae54efce5bcd5ad295d58ca6a
willyg23 <grinerwill5@gmail.com>
2024-02-27 11:28:53 -0600

    updated the choere part

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,18 +11,33 @@ from cohere.responses.classify import Example
 
 
 examples=[
-  Example("How do I ride a tractor", "riding fukin tractors boa"),
+  Example("How do I find my insurance policy?", "Finding policy details"),
+  Example("How do I download a copy of my insurance policy?", "Finding policy details"),
+  Example("How do I find my policy effective date?", "Finding policy details"),
+  Example("When does my insurance policy end?", "Finding policy details"),
+  Example("Could you please tell me the date my policy becomes effective?", "Finding policy details"),
   Example("How do I sign up for electronic filing?", "Change account settings"),
   Example("How do I change my policy?", "Change account settings"),
   Example("How do I sign up for direct deposit?", "Change account settings"),
   Example("I want direct deposit. Can you help with that?", "Change account settings"),
-  Example("Could you deposit money into my account rather than mailing me a physical cheque?", "Change account settings")
+  Example("Could you deposit money into my account rather than mailing me a physical cheque?", "Change account settings"),
+  Example("How do I file an insurance claim?", "Filing a claim and viewing status"),
+  Example("How do I file a reimbursement claim?", "Filing a claim and viewing status"),
+  Example("How do I check my claim status?", "Filing a claim and viewing status"),
+  Example("When will my claim be reimbursed?", "Filing a claim and viewing status"),
+  Example("I filed my claim 2 weeks ago but I still haven’t received a deposit for it.", "Filing a claim and viewing status"),
+  Example("I want to cancel my policy immediately! This is nonsense.", "Cancelling coverage"),
+  Example("Could you please help my end my insurance coverage? Thank you.",
+  "Cancelling coverage"),
+  Example("Your service sucks. I’m switching providers. Cancel my coverage.", "Cancelling coverage"),
+  Example("Hello there! How do I cancel my coverage?", "Cancelling coverage"),
+  Example("How do I delete my account?", "Cancelling coverage")
 ]
 
-inputs = [" I want to change my password", 
-          "Does my policy cover prescription medication?",
-          "I want to ride a fukin tractor boa"
-         ]
+
+inputs=[" I want to change my password”,
+        "Does my policy cover prescription medication?"
+        ]
 
 response = co.classify(  
     model='large',  
@@ -96,43 +111,6 @@ print(response.classifications)
       ],
       "labels": {
          "Finding policy details": {
-          "confidence": 0.75
-        },
-        "Change account settings": {
-          "confidence": 0.15
-        },
-        "Filing a claim and viewing status": {
-          "confidence": 0.05
-        },
-         "Cancelling coverage": {
-          "confidence": 0.05
-        }
-      }
-    },
-    {
-      "text":  "I want to ride a fukin tractor boa",
-      "prediction": "riding fukin tractors boa",
-      "confidence": 0.75,
-      "confidences": [
-        {
-          "option": "Finding policy details",
-          "confidence": 0.75
-        },
-        {
-          "option": "Change account settings",
-          "confidence": 0.15
-        },
-        {
-          "option": "Filing a claim and viewing status",
-          "confidence": 0.05
-        },
-       {
-          "option":  "Cancelling coverage",
-          "confidence": 0.05
-        }
-      ],
-      "labels": {
-         "riding fukin tractors boa": {
           "confidence": 0.75
         },
         "Change account settings": {
