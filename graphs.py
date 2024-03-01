7612c123a8f049083d1011598793dd428d609704
lvrd_ade <fadahunsiadeife11@gmail.com>
2024-02-29 23:44:02 -0600

    graphs update

--- graphs.py
+++ graphs.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 from globals import *
 
@@ -13,8 +14,22 @@ def main():
     y1, y2, y3, y4 = 'AvgT', 'WindSpeed', 'Humidity', 'Precipitation'
     chosen_y  = y1
 
+    #df['Date'] = df['Year'].astype(str) + '-' + df['Month'].astype(str)
+
+    # Create the heatmap
+    #sns.heatmap(data=df, x='Date', y='Rainfall', cmap='coolwarm')
+    #plt.show()
+
+
     #bar_graph(chosen_x, chosen_y)
-    bar_graph(x5, y2)
+    #bar_graph(y4, x2)
+    pivot_df = df.pivot_table(index='Month', columns=['Day'], values='Precipitation', aggfunc='mean')
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(pivot_df, cmap='YlGnBu', cbar_kws={'label': 'Average Precipitation (mm)'})
+    plt.title('Average Precipitation by Day and Month (2006-2023)')
+    plt.xlabel('Month - Day')
+    plt.ylabel('Year')
+    plt.show()
 
 
 def bar_graph(x, y):
@@ -26,6 +41,14 @@ def bar_graph(x, y):
     plt.grid(True)
     plt.show()
 
+def heat_map(x, y):
+    pivot_df = df.pivot_table(index='Year', columns=['Month', 'Day'], values='Precipitation', aggfunc='mean')
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(pivot_df, cmap='YlGnBu', cbar_kws={'label': 'Average Precipitation (mm)'})
+    plt.title('Average Precipitation by Day and Month (2006-2023)')
+    plt.xlabel('Month - Day')
+    plt.ylabel('Year')
+    plt.show()
 
 
 main()
