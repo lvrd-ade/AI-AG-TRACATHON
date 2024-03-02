1c14d81b7fcdbfabe3fde7dd3301d1de5de179ea
willyg23 <grinerwill5@gmail.com>
2024-03-01 23:42:10 -0600

    commented out annoying stuff and fixed some basic typos and stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,122 +49,122 @@ st.markdown(page_bg_image, unsafe_allow_html=True)
 #------------------------------------------ Cohort Stuff
 co = cohere.Client(COHERE_API_KEY)
 
-from cohere.responses.classify import Example
+# from cohere.responses.classify import Example
 
-examples=[
-  Example("How do I find my insurance policy?", "Finding policy details"),
-  Example("How do I download a copy of my insurance policy?", "Finding policy details"),
-  Example("How do I find my policy effective date?", "Finding policy details"),
-  Example("When does my insurance policy end?", "Finding policy details"),
-  Example("Could you please tell me the date my policy becomes effective?", "Finding policy details"),
-  Example("How do I sign up for electronic filing?", "Change account settings"),
-  Example("How do I change my policy?", "Change account settings"),
-  Example("How do I sign up for direct deposit?", "Change account settings"),
-  Example("I want direct deposit. Can you help with that?", "Change account settings"),
-  Example("Could you deposit money into my account rather than mailing me a physical cheque?", "Change account settings"),
-  Example("How do I file an insurance claim?", "Filing a claim and viewing status"),
-  Example("How do I file a reimbursement claim?", "Filing a claim and viewing status"),
-  Example("How do I check my claim status?", "Filing a claim and viewing status"),
-  Example("When will my claim be reimbursed?", "Filing a claim and viewing status"),
-  Example("I filed my claim 2 weeks ago but I still haven’t received a deposit for it.", "Filing a claim and viewing status"),
-  Example("I want to cancel my policy immediately! This is nonsense.", "Cancelling coverage"),
-  Example("Could you please help my end my insurance coverage? Thank you.",
-  "Cancelling coverage"),
-  Example("Your service sucks. I’m switching providers. Cancel my coverage.", "Cancelling coverage"),
-  Example("Hello there! How do I cancel my coverage?", "Cancelling coverage"),
-  Example("How do I delete my account?", "Cancelling coverage")
-]
+# examples=[
+#   Example("How do I find my insurance policy?", "Finding policy details"),
+#   Example("How do I download a copy of my insurance policy?", "Finding policy details"),
+#   Example("How do I find my policy effective date?", "Finding policy details"),
+#   Example("When does my insurance policy end?", "Finding policy details"),
+#   Example("Could you please tell me the date my policy becomes effective?", "Finding policy details"),
+#   Example("How do I sign up for electronic filing?", "Change account settings"),
+#   Example("How do I change my policy?", "Change account settings"),
+#   Example("How do I sign up for direct deposit?", "Change account settings"),
+#   Example("I want direct deposit. Can you help with that?", "Change account settings"),
+#   Example("Could you deposit money into my account rather than mailing me a physical cheque?", "Change account settings"),
+#   Example("How do I file an insurance claim?", "Filing a claim and viewing status"),
+#   Example("How do I file a reimbursement claim?", "Filing a claim and viewing status"),
+#   Example("How do I check my claim status?", "Filing a claim and viewing status"),
+#   Example("When will my claim be reimbursed?", "Filing a claim and viewing status"),
+#   Example("I filed my claim 2 weeks ago but I still haven’t received a deposit for it.", "Filing a claim and viewing status"),
+#   Example("I want to cancel my policy immediately! This is nonsense.", "Cancelling coverage"),
+#   Example("Could you please help my end my insurance coverage? Thank you.",
+#   "Cancelling coverage"),
+#   Example("Your service sucks. I’m switching providers. Cancel my coverage.", "Cancelling coverage"),
+#   Example("Hello there! How do I cancel my coverage?", "Cancelling coverage"),
+#   Example("How do I delete my account?", "Cancelling coverage")
+# ]
 
-inputs = ["I want to change my password", 
-          "Does my policy cover prescription medication?" 
-         ]
+# inputs = ["I want to change my password", 
+#           "Does my policy cover prescription medication?" 
+#          ]
 
-response = co.classify(  
-    model='large',  
-    inputs=inputs,  
-    examples=examples)
+# response = co.classify(  
+#     model='large',  
+#     inputs=inputs,  
+#     examples=examples)
 
-print(response.classifications)
+# print(response.classifications)
 
-{
-  "results": [
-    {
-      "text": "I want to change my password",
-      "prediction": "Change account settings",
-      "confidence": 0.82,
-      "confidences": [
-        {
-          "option": "Finding policy details",
-          "confidence": 0.05
-        },
-        {
-          "option": "Change account settings ",
-          "confidence": 0.82
-        },
-        {
-          "option": "Filing a claim and viewing status",
-          "confidence": 0.05
-        },
-       {
-          "option":  "Cancelling coverage",
-          "confidence": 0.08
-        }
-      ],
+# {
+#   "results": [
+#     {
+#       "text": "I want to change my password",
+#       "prediction": "Change account settings",
+#       "confidence": 0.82,
+#       "confidences": [
+#         {
+#           "option": "Finding policy details",
+#           "confidence": 0.05
+#         },
+#         {
+#           "option": "Change account settings ",
+#           "confidence": 0.82
+#         },
+#         {
+#           "option": "Filing a claim and viewing status",
+#           "confidence": 0.05
+#         },
+#        {
+#           "option":  "Cancelling coverage",
+#           "confidence": 0.08
+#         }
+#       ],
       
-      "labels": {
-       "Finding policy details": {
-          "confidence": 0.05
-        },
-        "Change account settings": {
-          "confidence": 0.82
-        },
-        "Filing a claim and viewing status": {
-          "confidence": 0.05
-        },
-         "Cancelling coverage": {
-          "confidence": 0.08
-        }
-      }
-    },
-    {
-      "text":  "Does my policy cover prescription medication?",
-      "prediction": "Finding policy details",
-      "confidence": 0.75,
-      "confidences": [
-        {
-          "option": "Finding policy details",
-          "confidence": 0.75
-        },
-        {
-          "option": "Change account settings",
-          "confidence": 0.15
-        },
-        {
-          "option": "Filing a claim and viewing status",
-          "confidence": 0.05
-        },
-       {
-          "option":  "Cancelling coverage",
-          "confidence": 0.05
-        }
-      ],
-      "labels": {
-         "Finding policy details": {
-          "confidence": 0.75
-        },
-        "Change account settings": {
-          "confidence": 0.15
-        },
-        "Filing a claim and viewing status": {
-          "confidence": 0.05
-        },
-         "Cancelling coverage": {
-          "confidence": 0.05
-        }
-      }
-    }
-  ]
-}
+#       "labels": {
+#        "Finding policy details": {
+#           "confidence": 0.05
+#         },
+#         "Change account settings": {
+#           "confidence": 0.82
+#         },
+#         "Filing a claim and viewing status": {
+#           "confidence": 0.05
+#         },
+#          "Cancelling coverage": {
+#           "confidence": 0.08
+#         }
+#       }
+#     },
+#     {
+#       "text":  "Does my policy cover prescription medication?",
+#       "prediction": "Finding policy details",
+#       "confidence": 0.75,
+#       "confidences": [
+#         {
+#           "option": "Finding policy details",
+#           "confidence": 0.75
+#         },
+#         {
+#           "option": "Change account settings",
+#           "confidence": 0.15
+#         },
+#         {
+#           "option": "Filing a claim and viewing status",
+#           "confidence": 0.05
+#         },
+#        {
+#           "option":  "Cancelling coverage",
+#           "confidence": 0.05
+#         }
+#       ],
+#       "labels": {
+#          "Finding policy details": {
+#           "confidence": 0.75
+#         },
+#         "Change account settings": {
+#           "confidence": 0.15
+#         },
+#         "Filing a claim and viewing status": {
+#           "confidence": 0.05
+#         },
+#          "Cancelling coverage": {
+#           "confidence": 0.05
+#         }
+#       }
+#     }
+#   ]
+# }
 
 
 
@@ -173,12 +173,12 @@ print(response.classifications)
 #------------------------------------ streamlit stuff
 
 
-st.title('Rainfall Predictions Utilizing Machinen Learning')
-st.write("Hello, world!")
+st.title('Rainfall Predictions Utilizing Machine Learning')
+# st.write("Hello, world!")
 
 #----------Cohere UI Stuff----------
 # User input area
-user_input = st.text_input("Enter your query:", "")
+# user_input = st.text_input("Enter your query:", "")
 # Button to trigger classification
 # if st.button('Submit'):
 #     if user_input:  # Check if there's input
@@ -276,6 +276,7 @@ if st.button('Predict Rainfall'):
     # print(prediction_label)
     if prediction_label == 'No Rain':
       st.success(f'The predicted rainfall category is: {prediction_label}', icon="🌵")
+      # print("goo goo doo doo")
     elif prediction_label == 'Light Rain':
       st.success(f'The predicted rainfall category is: {prediction_label}', icon="💧")
     elif prediction_label == 'Moderate Rain':
